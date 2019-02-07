c8ab9c4a92d004b37142c060fa0e46eecc8a5ed7
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-02-07 13:38:36 -0600

    Initial Commit

---
+++ src/Todo.java
@@ -0,0 +1,18 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-07
+OU ID_______________: 113467371
+OU EMAIL____________: christian.j.mcbae-1@ou.edu
+
+Tasks:
+1. Finish Animal equals method
+2. XXX
+
+Estimated Time (minutes):
+1. 5
+2. XXx
+
+Actual Time (minutes):
+1. 10
+2. XXX
+*/}
